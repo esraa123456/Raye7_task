de05f5812f97b6cbf1707cfd0b46a4e994d8f354
esraa123456 <esraadarweesh1995@gmail.com>
2019-12-28 22:31:00 +0200

    fixing models

--- app/models/pickup.rb
+++ app/models/pickup.rb
@@ -11,4 +11,7 @@
 #
 
 class Pickup < ApplicationRecord
+    belongs_to :passenger, class_name: 'User', foreign_key: :passenger_id
+    belongs_to :source_place, class_name: 'Place', foreign_key: :source_id
+    belongs_to :destination_place, class_name: 'Place', foreign_key: :destination_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  extend Enumerize
+
+  enumerize :role, in: {:driver => 1, :passenger => 2}
+  validates :first_name, :last_name, presence: true
+  validates :phone, uniqueness: true
+  
+  has_many :trips, foreign_key: 'driver_id', dependent: :destroy
+  has_many :pickups, foreign_key: 'passenger_id', dependent: :destroy
 end
